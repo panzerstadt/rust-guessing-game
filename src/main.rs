ab760cc75d92199c449d80dc09be621506648ebd
panzerstadt <tliqun@gmail.com>
2022-11-14 23:04:20 +0900

    number guessing game

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    let tries = 10;
+    println!("Guess a number!! you have {tries} tries");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    if rand::random() {
+        println!("psst! -> {secret_number}");
+    }
+
+    let mut counter = 1;
+    // you can return value from a while loop!
+    let wth = loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim() == "quit" {
+            break "Quit";
+        }
+
+        // match is like the switch case, but you can use it anywhere!
+        let guess = match guess.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+
+        let remaining = tries - counter;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small! {remaining} guesses left.");
+                counter += 1;
+            }
+            Ordering::Greater => {
+                println!("Too big! {remaining} guesses left.");
+                counter += 1;
+            }
+            Ordering::Equal => {
+                println!("You are correct!!!! you get a fuzzy feeling in your heart.");
+                break "Win";
+            }
+        };
+
+        if counter > tries {
+            break "Lose";
+        }
+    };
+    println!("----------\nYou {wth}!")
+}
